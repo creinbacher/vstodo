5304fdbbb7fad499903a9c0e7cfea977bde2f09d
Christopher Reinbacher <reinbacher@gmail.com>
2021-01-06 11:06:06 +0100

    added database

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,24 @@
+import 'reflect-metadata'
 import express from 'express'
-
+import {createConnection} from 'typeorm'
+import { __prod__ } from './constants';
+import {join} from 'path';
+import { User } from './entities/User';
 
 const main = async() =>{
+    await createConnection({
+        type:'postgres',
+        database: 'vstodo',
+        username: 'vstodo',
+        password: 'vstodo',
+        entities: [join(__dirname, './entities/*.*')],
+        logging: !__prod__,
+        synchronize: !__prod__
+    });
+
+    const user = await User.create({name: 'bob'}).save();
+    console.log({user});
+
     const app = express();
     app.get('/', (_, res) =>{
         res.send("Hello");
